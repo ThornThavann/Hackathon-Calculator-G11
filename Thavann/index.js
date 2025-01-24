0473ac93aa85a0e7dd5c75fb5701167a5988af03
ThornThavann <thavann.thorn@institute.pse.ngo>
2025-01-24 13:17:07 +0700

    Calculator

---
+++ Thavann/index.js
@@ -0,0 +1,16 @@
+const display = document.getElementById("display");
+function oppendToDisplay(input) {
+  display.value += input;
+}
+function clearDisplay() {
+  display.value = "";
+}
+function calculate() {
+  display.value = eval(display.value);
+}
+function Back() {
+  display.value = display.value.slice(0, -1);
+}
+function divide(input) {
+  display.value /= input;
+}
