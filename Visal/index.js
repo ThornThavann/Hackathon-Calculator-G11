7f415c7c42af19a8a353c713261a040223a947c4
viaslphoung <visalphoung22@gmail.com>
2025-02-16 10:10:07 +0700

    new commit

--- Visal/index.js
+++ Visal/index.js
@@ -2,24 +2,41 @@
 
 const display = document.getElementById("display");
 
-function appendToDisplay(input){
-    display.value += input;
+function appendToDisplay(input) {
+  display.value += input;
 }
 
-function clearDisplay(){
-    display.value = "";
+function clearDisplay() {
+  display.value = "";
 }
 
-function calculate(){
-    try {
-        display.value = eval(display.value);
-    } catch (error) {
-        display.value = "Error";
-    }
+function calculate() {
+  try {
+    display.value = eval(display.value);
+  } catch (error) {
+    display.value = "Error";
+  }
 }
+document.querySelector(".sqrt-btn").addEventListener("click", function () {
+  appendToDisplay("Math.sqrt(");
+});
 
+document.querySelector(".sin-btn").addEventListener("click", function () {
+  appendToDisplay("Math.sin(");
+});
 
+document.querySelector(".cos-btn").addEventListener("click", function () {
+  appendToDisplay("Math.cos(");
+});
 
+document.querySelector(".tan-btn").addEventListener("click", function () {
+  appendToDisplay("Math.tan(");
+});
 
+document.querySelector(".log-btn").addEventListener("click", function () {
+  appendToDisplay("Math.log(");
+});
 
-
+document.querySelector(".exp-btn").addEventListener("click", function () {
+  appendToDisplay("Math.exp(");
+});
